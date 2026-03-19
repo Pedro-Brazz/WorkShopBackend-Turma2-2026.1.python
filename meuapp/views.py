530f0c09c7d7803a00cdb7c2940039f7b0dd8b77
Pedro-Brazz <pedrobraz090987@gmail.com>
2026-03-19 16:02:33 -0300

    📚 :books: criação de um crud básico utilizando FBV.

--- meuapp/views.py
+++ meuapp/views.py
@@ -8,7 +8,7 @@ from.forms import PessoaForm
 
 def listar_pessoas(request):
     pessoas = Pessoa.objects.all()
-    return render(request, 'meuapp/list.html', {'pessoa' : pessoas})
+    return render(request, 'meuapp/list.html', {'pessoas' : pessoas})
 
 def criar_pessoa(request):
     if request.method == 'POST':
@@ -20,6 +20,25 @@ def criar_pessoa(request):
     else:
         form = PessoaForm()
     return render(request, 'meuapp/form.html', {'form' : form})
+
+def atualizar_pessoa(request, pk):
+    pessoa = Pessoa.objects.get(pk=pk)
+    if request . method == 'POST':
+        form = PessoaForm(request.POST, instance=pessoa)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_pessoas')
+        
+    else:
+        form = PessoaForm(instance=pessoa)
+    return render(request, 'meuapp/form.html', {'form': form})
+
+def deletar_pessoa(request, pk):
+    pessoa = Pessoa.objects.get(pk=pk)
+    if request.method == "POST":
+        pessoa.delete()
+        return redirect('listar_pessoas')
+    return render(request, 'meuapp/confirmar_delete.html', {'pessoa': pessoa})
 
 
 
